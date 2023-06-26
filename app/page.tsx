3caff158418dff5105f83da5e592ecc42dd41f92
João Gabriel Loureiro <102371975+jxjxloureiro@users.noreply.github.com>
2023-06-26 18:09:09 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <CartProvider>
       <Navbar/>
-      <div class="container my-12 mx-auto px-4 md:px-12">
-        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+      <div className="container my-12 mx-auto px-4 md:px-12">
+        <div className="flex flex-wrap -mx-1 lg:-mx-4">
           <ProductCard />
         </div>
       </div>
